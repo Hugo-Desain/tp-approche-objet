e9d20fb87dce44293196bca2c99d76b075be9880
hdesain-diginamic <hugodesain@gmail.com>
2024-10-04 17:02:39 +0200

    TP 4 terminé

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+
+        String chaine = "12";
+        int number = Integer.parseInt(chaine);
+
+        int a = 3;
+        int b = 5;
+        Integer.max(a, b);
+
+        System.out.println(Integer.max(a, b));
+    };
+}
